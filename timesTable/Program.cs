492d617569197d33da356542d4887e7fd4b3a0c0
kclaka <kennyigbechi@gmail.com>
2020-03-31 08:35:45 -0700

    Add project files.

---
+++ timesTable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace timesTable
+{
+    class Program
+    {
+        static void TimesTable(byte number)
+        {
+            Console.WriteLine($"This is the {number} times table:");
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{number} * {i} = {number * i}");
+            }
+        }
+
+        static void RunTimesTable()
+        {
+            bool isnumber;
+
+            do
+            {
+                Console.WriteLine("Enter a Number between 0 and 255");
+                isnumber = byte.TryParse(Console.ReadLine(), out byte number);
+
+                if (isnumber)
+                {
+                    TimesTable(number);
+                }
+                else
+                {
+                    Console.WriteLine("You didn't enter a real number");
+                }
+
+            } while (isnumber);
+
+        }
+
+        static void Main(string[] args)
+        {
+            RunTimesTable();
+        }
+
+    }
+
+}
+
